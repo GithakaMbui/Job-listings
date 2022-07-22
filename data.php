a1aac3bd08f96dc17a9e0ac7f791b5bcd3bf4cc6
Githaka <githakamaru@gmail.com>
2022-07-22 03:12:15 +0300

    setup homepage and single page

---
+++ data.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    [
+        'title' => 'Consultant Neurologist',
+        'tags' => 'specialist, Multiple, Sclerosis ',
+        'company' => 'Acme Corp',
+        'location' => 'Boston, MA',
+        'email' => 'email1@email.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+    ],
+    [
+        'title' => 'Consultant Anaesthetics',
+        'tags' => 'Consultant,Anaesthetics',
+        'company' => 'Stark Industries',
+        'location' => 'New York, NY',
+        'email' => 'email2@email.com',
+        'website' => 'https://www.starkindustries.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+    ],
+    [
+        'title' => 'Consultant Psychiatrist ',
+        'tags' => 'Perinatal,Mental,Health',
+        'company' => 'Wayne Enterprises',
+        'location' => 'Gotham, NY',
+        'email' => 'email3@email.com',
+        'website' => 'https://www.wayneenterprises.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+    ],
+    [
+        'title' => 'Senior Clinical Fellow in Paediatric Oncology',
+        'tags' => 'CLINICAl, FELLOW, DRUG, DEVELOPMENT, TRANSLATIONAL,PAEDIATRIC, ONCOLOGY',
+        'company' => 'Skynet Systems',
+        'location' => 'Newark, NJ',
+        'email' => 'email4@email.com',
+        'website' => 'https://www.skynet.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+    ],
+    [
+        'title' => 'Junior Developer',
+        'tags' => 'laravel, php, javascript',
+        'company' => 'Wonka Industries',
+        'location' => 'Boston, MA',
+        'email' => 'email4@email.com',
+        'website' => 'https://www.wonka.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+    ]
+];
